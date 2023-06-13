76f5203342d5244523382d61fd7b1ba6a9ee510b
skutner <sava.dumitru.daniel@gmail.com>
2023-06-13 17:48:00 +0300

    using aws v 4.47

--- deployments/terraform/aws-eks-create-infrastructure-pla/versions.tf
+++ deployments/terraform/aws-eks-create-infrastructure-pla/versions.tf
@@ -5,7 +5,7 @@ terraform {
   required_providers {
     # default tags supported from > 3.38 https://www.hashicorp.com/blog/default-tags-in-the-terraform-aws-provider
     # tags for aws_elasticache_parameter_group > 3.43
-    aws        = ">= 4.47"
+    aws        = "= 4.47"
     kubernetes = ">= 2.10"
   }
 }
